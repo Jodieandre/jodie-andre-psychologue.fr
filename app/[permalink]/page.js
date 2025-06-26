8f2d81153ea9ba4ffb7dd0fc2b8e2e1b1a91c378
johackim <contact@johackim.com>
2025-06-27 00:19:09 +0200

    feat: update website

--- app/[permalink]/page.js
+++ app/[permalink]/page.js
@@ -33,7 +33,7 @@ const Page = async ({ params }) => {
                 />
 
                 <section className="px-6 pt-46 pb-8 lg:px-8 text-white">
-                    <article className="mx-auto max-w-3xl text-base/7 text-gray-700 prose prose-headings:text-white prose-strong:text-white text-white">
+                    <article className="mx-auto max-w-3xl text-base/7 text-gray-700 prose prose-headings:text-white prose-strong:text-white prose-blockquote:text-white prose-a:text-white prose-code:text-white text-white">
                         <MDXRemote source={markdown} options={options} />
                     </article>
                 </section>
--- app/not-found.js
+++ app/not-found.js
@@ -1,13 +1,27 @@
 import Link from 'next/link';
+import Header from '../components/header';
+import Footer from '../components/footer';
 
 export default () => (
-    <div className="mx-auto max-w-7xl px-6 lg:px-8 h-full flex items-center">
-        <div className="mx-auto max-w-2xl text-center text-white">
-            <h1 className="text-2xl">Erreur 404</h1>
-            <p className="mt-2 text-lg font-medium">La page que vous recherchez n'existe pas.</p>
-            <div className="mt-4">
-                <Link href="/" className="underline">Retourner à l'accueil</Link>
+    <>
+        <Header />
+        <main className="flex flex-col flex-1 justify-center after:fixed after:inset-0 after:bg-sky-950/80 after:-z-10 after:content-['']">
+            <img
+                alt="background"
+                src="/background.jpg"
+                className="absolute inset-0 -z-10 h-full w-full object-cover fixed"
+            />
+
+            <div className="mx-auto max-w-7xl px-6 lg:px-8 h-full flex items-center">
+                <div className="mx-auto max-w-2xl text-center text-white">
+                    <h1 className="text-3xl font-bold">Erreur 404</h1>
+                    <p className="mt-2 text-lg font-medium">La page que vous recherchez n'existe pas.</p>
+                    <div className="mt-4">
+                        <Link href="/" className="underline">Retourner à l'accueil</Link>
+                    </div>
+                </div>
             </div>
-        </div>
-    </div>
+        </main>
+        <Footer />
+    </>
 );
