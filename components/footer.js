8e1d7188ee033f8ab7d383c8a963f181076af399
johackim <contact@johackim.com>
2025-06-29 14:48:54 +0200

    feat: update website

--- components/footer.js
+++ components/footer.js
@@ -1,7 +1,7 @@
 import settings from '../settings.json';
 
-export default ({ className = 'text-white' }) => (
-    <footer className={`flex items-center justify-between p-6 sm:p-8 ${className}`}>
+export default () => (
+    <footer className={`flex items-center justify-between p-6 sm:p-8 text-${settings.footer.textColor}`}>
         <div>
             <span>&copy;</span>
             <span className="ml-2">
@@ -10,7 +10,7 @@ export default ({ className = 'text-white' }) => (
             </span>
         </div>
         <nav className="grid grid-flow-col gap-3 items-center">
-            {settings.social.map(({ name, link, linkColor, icon }) => (
+            {settings.footer.social.map(({ name, link, linkColor, icon }) => (
                 <a href={link} aria-label={name} key={name}>
                     <div
                         className={`h-5 w-5 bg-${linkColor}`}
